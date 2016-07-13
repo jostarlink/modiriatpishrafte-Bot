dfacc071a95210c27c06e9f2464526f080860d0b
iranianbot <clashid000@gmail.com>
2016-07-13 01:07:51 -0700

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -228,7 +228,7 @@ function create_config( )
       "weather",
       "xkcd",
       "youtube" },
-    sudo_users = {our_id},
+    sudo_users = {179591911},
     disabled_channels = {}
   }
   serialize_to_file(config, './data/config.lua')
